c36cf168c1f01887fc9a53cf10fcca9660c2b522
Oonuliina <91269427+Oonuliina@users.noreply.github.com>
2023-03-27 18:08:12 +0300

    newmessage functions
    New messagen submit-nappi tallentaa läheteyt tiedot json tiedostoon

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const fs = require('fs')
-
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public/guestbooksite/'))
 
 app.get('/', (req, res) => {
@@ -12,10 +13,53 @@ app.get('/', (req, res) => {
 app.get('/guestbook', (req, res) => {
     res.sendFile(__dirname + '/public/guestbook.html')
 })
+ 
+/* app.get('/guestbook', (req, res) => {
+    var data = require('./json-data.json')
+    
+    var results = '<table border="1">'+
+                    '<tr>'+
+                    '<th>Username</th>'+
+                    '<th>Country</th>'+
+                    '<th>Message</th>'+
+                    '</tr>'
+
+    for (var i=0; i < data.length; i++){
+        results +=
+        '<tr>'+
+        '<td>'+data[i].username+'</td>'+
+        '<td>'+data[i].country+'</td>'+
+        '<td>'+data[i].message+'</td>'+
+        '</tr>'
+      }
+
+      res.send(results)
+}) */
 
 app.get('/newmessage', (req, res) => {
     res.sendFile(__dirname + '/public/newmessage.html')
 })
+
+app.post('/newmessage', (req, res) => {
+    var data = require('./json-data.json')
+
+    var formData = {
+        username: req.body.uname,
+        country: req.body.country,
+        message: req.body.message,
+    }
+
+    data.push(formData)
+
+    var jsonStr = JSON.stringify(data, "", 1)
+
+    fs.writeFile('json-data.json', jsonStr, (err) => {
+        if (err) throw err
+    })
+
+    res.redirect('/guestbook')
+})
+
 
 app.get('/ajaxmessage', (req, res) => {
     res.sendFile(__dirname + '/public/ajaxmessage.html')
