c1581ae9961b889f6942643cd9dcdd17dbbe6cd6
Oonuliina <91269427+Oonuliina@users.noreply.github.com>
2023-03-31 02:05:38 +0300

    guestbook and ajaxmessage
    Guestbook finished, ajaxmessage almost done

--- app.js
+++ app.js
@@ -3,43 +3,31 @@ const bodyParser = require('body-parser')
 const fs = require('fs')
 const app = express()
 
+// otetaan käyttöön staattinen sisältö public-kansiosta
+app.use(express.static('public'))
+
+app.locals.pretty = true;
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('public/guestbooksite/'))
 
+app.set('view engine', 'ejs')
+
+// renderöidään etusivu
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
+    res.render('pages/index')
 })
 
+// renderöidään guestbook-sivu ja siihen json-muotoista dataa
 app.get('/guestbook', (req, res) => {
-    res.sendFile(__dirname + '/public/guestbook.html')
+var datas = require('./json-data.json')
+    res.render('pages/guestbook', {messages: datas}) 
 })
- 
-/* app.get('/guestbook', (req, res) => {
-    var data = require('./json-data.json')
-    
-    var results = '<table border="1">'+
-                    '<tr>'+
-                    '<th>Username</th>'+
-                    '<th>Country</th>'+
-                    '<th>Message</th>'+
-                    '</tr>'
 
-    for (var i=0; i < data.length; i++){
-        results +=
-        '<tr>'+
-        '<td>'+data[i].username+'</td>'+
-        '<td>'+data[i].country+'</td>'+
-        '<td>'+data[i].message+'</td>'+
-        '</tr>'
-      }
-
-      res.send(results)
-}) */
-
+// renderöidään newmessage-sivu
 app.get('/newmessage', (req, res) => {
-    res.sendFile(__dirname + '/public/newmessage.html')
+    res.render('pages/newmessage')
 })
 
+// lisätään syötetty data json tiedostoon
 app.post('/newmessage', (req, res) => {
     var data = require('./json-data.json')
 
@@ -56,17 +44,39 @@ app.post('/newmessage', (req, res) => {
     fs.writeFile('json-data.json', jsonStr, (err) => {
         if (err) throw err
     })
-
+    // uudelleenohjataan guestbook-sivulle viestin syötteen jälkeen
     res.redirect('/guestbook')
 })
 
-
+// renderöidään ajaxmessage-sivu
 app.get('/ajaxmessage', (req, res) => {
-    res.sendFile(__dirname + '/public/ajaxmessage.html')
+    res.render('pages/ajaxmessage')
 })
 
+// lähetetään ajaxmessage-sivulta saatu syöte
+app.post('/ajaxmessage', (req, res) => {
+    var username = req.body.uname
+    var country = req.body.country
+    var message = req.body.message
+
+    var results = '<table>'+
+    '<tr>'+
+    '<th>Username</th>'+
+    '<th>Country</th>'+
+    '<th>Message</th>'+
+    '</tr>'+
+    '<tr>'+
+    '<td>'+username+'</td>'+
+    '<td>'+country+'</td>'+
+    '<td>'+message+'</td>'+
+    '</tr>'
+
+    res.send(results)  
+})
+
+// vihreilmoitus, jos jokin menee pieleen
 app.get('*', (req, res) => {
     res.send('Cant find the requested page', 404)
 })
-
+// kuunnellaan porttia 5000
 app.listen(5000)
