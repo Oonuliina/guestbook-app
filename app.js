eb653016171b18b59acb71c797ca3be7cd1a571c
Oonuliina <91269427+Oonuliina@users.noreply.github.com>
2023-03-31 19:29:26 +0300

    comments + small fixes
    Comments and small details + fixes

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+// otetaan express käyttöön
 const express = require('express')
-const bodyParser = require('body-parser')
-const fs = require('fs')
 const app = express()
+
+// otetaan tiedostojen muokkaus ja luku käyttöön
+const fs = require('fs')
+
+// otetaan bodyparser käyttöön
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // otetaan käyttöön staattinen sisältö public-kansiosta
 app.use(express.static('public'))
 
-app.locals.pretty = true;
-app.use(bodyParser.urlencoded({ extended: true }))
-
+// otetaan EJS käyttöön
 app.set('view engine', 'ejs')
+
+// pakotetaan sivupohja tuottamaan kaunista HTML:ää
+app.locals.pretty = true;
 
 // renderöidään etusivu
 app.get('/', (req, res) => {
@@ -18,7 +25,7 @@ app.get('/', (req, res) => {
 
 // renderöidään guestbook-sivu ja siihen json-muotoista dataa
 app.get('/guestbook', (req, res) => {
-var datas = require('./json-data.json')
+    const datas = require('./json-data.json')
     res.render('pages/guestbook', {messages: datas}) 
 })
 
@@ -27,20 +34,21 @@ app.get('/newmessage', (req, res) => {
     res.render('pages/newmessage')
 })
 
-// lisätään syötetty data json tiedostoon
+// lisätään syötetty data JSON tiedostoon
 app.post('/newmessage', (req, res) => {
-    var data = require('./json-data.json')
-
-    var formData = {
+    // otetaan JSON-tiedosto käyttöön
+    const data = require('./json-data.json')
+    // bodyparser in action, otetaan sen avulla tiedot html formista
+    const formData = {
         username: req.body.uname,
         country: req.body.country,
         message: req.body.message,
     }
-
+    // lisätään olio
     data.push(formData)
-
-    var jsonStr = JSON.stringify(data, "", 1)
-
+    // muutetaan olio JSON muotoon ja tehdään siihen kaunis asettelu
+    const jsonStr = JSON.stringify(data, "", 1)
+    // kirjoitetaan JSON tiedosto levylle ja tarkistetaan errorien varalta
     fs.writeFile('json-data.json', jsonStr, (err) => {
         if (err) throw err
     })
@@ -52,30 +60,32 @@ app.post('/newmessage', (req, res) => {
 app.get('/ajaxmessage', (req, res) => {
     res.render('pages/ajaxmessage')
 })
-
-var results = '<table>'+
-    '<tr>'+
-    '<th>Username</th>'+
-    '<th>Country</th>'+
-    '<th>Message</th>'+
-    '</tr>';
+    // luodaan ajaxmessage sivulle taulukko, johon tulee sinne kirjoitetut tiedot näkyviin
+    let results = '<table>'+
+        '<tr>'+
+        '<th>Username</th>'+
+        '<th>Country</th>'+
+        '<th>Message</th>'+
+        '</tr>';
 
 // lähetetään ajaxmessage-sivulta saatu syöte
 app.post('/ajaxmessage', (req, res) => {
-    var username = req.body.uname
-    var country = req.body.country
-    var message = req.body.message
+    // tallennetaan lomakkeen tiedot muuttujiin
+    const username = req.body.uname
+    const country = req.body.country
+    const message = req.body.message
     
+    // lisätään taulukkoon uusi rivi, jossa lukee syötetyt tiedot
     results+='<tr>'+
     '<td>'+username+'</td>'+
     '<td>'+country+'</td>'+
     '<td>'+message+'</td>'+
     '</tr>'
-
+    // välitetään palvelimen antama "tulos" takaisin selaimelle
     res.send(results)  
 })
 
-// vihreilmoitus, jos jokin menee pieleen
+// vihreilmoitus, jos jokin menee pieleen reittejä haettaessa
 app.get('*', (req, res) => {
     res.send('Cant find the requested page', 404)
 })
